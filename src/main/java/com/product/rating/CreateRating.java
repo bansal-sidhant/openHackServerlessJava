5189a403fcaf0566c1df17c2beb9b5e2fdbbd929
bansal-sidhant <sidb20@gmail.com>
2022-03-30 11:45:40 +0530

    User & Product Validation

--- src/main/java/com/product/rating/CreateRating.java
+++ src/main/java/com/product/rating/CreateRating.java
@@ -12,14 +12,25 @@ import com.microsoft.azure.functions.annotation.HttpTrigger;
 import org.apache.commons.text.StringEscapeUtils;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.UUID;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Azure Functions with HTTP Trigger.
  */
 public class CreateRating {
     /**
-     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
+     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it
+     * using "curl" command in bash:
      * 1. curl -d "HTTP Body" {your host}/api/HttpExample
      * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
      */
@@ -33,29 +44,82 @@ public class CreateRating {
 
     @FunctionName("createRating")
     public HttpResponseMessage run(
-            @HttpTrigger(
-                name = "req",
-                methods = {HttpMethod.POST},
-                authLevel = AuthorizationLevel.ANONYMOUS)
-                HttpRequestMessage<Optional<String>> request,
+            @HttpTrigger(name = "req", methods = {
+                    HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
-        //CreateRating rating = new CreateRating("demo","demo","demo","demo","demo",5,"demo");
-        // Parse query parameter
-       JSONObject json = new JSONObject(StringEscapeUtils.unescapeJava(request.getBody().get()));
 
-        // CreateRating rating = request.getBody();
-        final String query = request.getQueryParameters().get("name");
-        //final String name = request.getBody().orElse(query);
+        JSONObject json = new JSONObject(StringEscapeUtils.unescapeJava(request.getBody().get()));
+        id = UUID.randomUUID().toString();
+        userId = json.getString("userId");
+        productId = json.getString("productId");
+        rating = json.getInt("rating");
+        locationName = json.getString("locationName");
+        userNotes = json.getString("userNotes");
+        timeStamp = LocalDateTime.now().toString();
+
+        // https://serverlessohapi.azurewebsites.net/api/GetUser?userId=cc20a6fb-a91f-4192-874d-132493685376
+        String userURL = "https://serverlessohapi.azurewebsites.net/api/GetUser?userId=" + userId;
+        boolean isUserValid = false;
+        
+        String productURL = "https://serverlessohapi.azurewebsites.net/api/GetProduct?productId=" + productId;
+        boolean isProductValid = false;
+      
+        try {
+            URL userIdUrl = new URL(userURL);
+            isUserValid = getResponse(userIdUrl,context);
+
+            URL productIdUrl = new URL(productURL);
+            isProductValid = getResponse(productIdUrl,context);
+
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        context.getLogger().info("Valid User "+isUserValid+ " Valid Prouduct "+isProductValid);
 
         if (json == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+                    .body("Please pass a name on the query string or in the request body").build();
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body(json).build();
+            return request.createResponseBuilder(HttpStatus.OK).body(createJson().toString()).build();
         }
     }
 
-    public boolean isValid(){
+    public boolean isValid() {
+        return false;
+    }
+
+    private JSONObject createJson() {
+        JSONObject responseJson = new JSONObject();
+        responseJson.put("id", id);
+        responseJson.put("userId", userId);
+        responseJson.put("productId", productId);
+        responseJson.put("timestamp", timeStamp);
+        responseJson.put("locationName", locationName);
+        responseJson.put("rating", rating);
+        responseJson.put("userNotes", userNotes);
+        return responseJson;
+    }
+
+    private boolean getResponse(URL url,ExecutionContext context) throws IOException {
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setDoOutput(true);
+
+        connection.connect();
+        int code = connection.getResponseCode();
+        context.getLogger().info("URL " +url);
+        context.getLogger().info("Response Code" +connection.getResponseCode());
+
+        if (code == 200) {
+            return true;
+        } 
+
         return false;
     }
 }
